027359098ff930536da03628cab2f63cc38d7fe2
Bryan Willis <willis.bryan@live.com>
2015-12-13 22:49:39 -0600

    Update wordpress-widgets-bootstrapped.php

--- wordpress-widgets-bootstrapped.php
+++ wordpress-widgets-bootstrapped.php
@@ -90,3 +90,31 @@ function wop_bootstrap_widget_output_filters( $widget_output, $widget_type, $wid
 }
 
 add_filter( 'widget_output', 'wop_bootstrap_widget_output_filters', 10, 3 ); // not sure if this should be hooked into an action... maybe init or widgets_init
+
+
+
+/** 
+ * Search form created seperately... to advanced for string replacement
+ */
+add_filter( 'get_search_form', 'wop_bootstrap_search_form');
+function wop_bootstrap_search_form() {
+    $value_or_placeholder = ( get_search_query() == '' ) ? 'placeholder' : 'value';
+    $label = 'Search';
+    $search_text = 'Search this website...';
+    $button_text = 'Search';
+$form = '<form method="get" class="search-form form-inline" action="'.home_url( '/' ).'" role="search">
+    <div class="form-group">
+        <label class="sr-only sr-only-focusable" for="bsg-search-form">'.esc_html( $label ).'</label>
+        <div class="input-group">
+            <input type="search" class="search-field form-control" id="search" name="s" '.$value_or_placeholder.'="'.esc_attr( $search_text ).'">
+            <span class="input-group-btn">
+                <button type="submit" class="search-submit btn btn-default">
+                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                    <span class="sr-only">'.esc_attr( $button_text ).'</span>
+                </button>
+            </span>
+        </div>
+    </div>
+</form>';
+    return $form;
+}
